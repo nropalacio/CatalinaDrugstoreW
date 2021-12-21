c186e4f19b586b41a8e4f947697ca4ef7f55d030
nropalacio <nrodrigo.palaciov@gmail.com>
2021-12-20 22:52:21 -0600

    dependencias de primefaces

---
+++ src/main/java/com/catalina/catalinadrugstorew/manage/InventarioBean.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.catalina.catalinadrugstorew.manage;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Nelson Palacios
+ */
+@ManagedBean(name = "clienteBean")
+@ViewScoped
+public class InventarioBean implements Serializable{
+    
+
+
+
+}
